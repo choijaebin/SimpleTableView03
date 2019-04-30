44490c4335d31ed8b65fc8838b8986b52753f086
D7703_16 <d7703_16@d7703-16ui-imac.local>
2019-04-30 14:22:05 +0900

    Initial Commit

---
+++ SimpleTableView03/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SimpleTableView03
+//
+//  Created by D7703_16 on 2019. 4. 30..
+//  Copyright © 2019년 201526109. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
